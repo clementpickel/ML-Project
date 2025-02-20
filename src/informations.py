9543943351c12d8812814124e43890539a8f6e5a
Clément Pickel <clement.pickel@epitech.eu>
2025-02-21 00:06:39 +0100

    add draw method aand a filter

--- src/informations.py
+++ src/informations.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 
 class Information:
@@ -19,11 +20,17 @@ class Information:
         self.data = self.handleMissing(self.data)
 
     def getData(self):
-        combined_df = pd.concat(
+        df = pd.concat(
             [pd.read_csv(file, na_values=["n/e"]) for file in self.csv_to_get],
             ignore_index=True
         )
-        return combined_df
+
+        # divide Time in two column
+        df['StartTime'] = df['MTU (CET/CEST)'].apply(lambda x: x.split(" - ")[0])
+        df['EndTime'] = df['MTU (CET/CEST)'].apply(lambda x: x.split(" - ")[1])
+        df['StartTime'] = pd.to_datetime(df['StartTime'], format="%d.%m.%Y %H:%M")
+        df['EndTime'] = pd.to_datetime(df['EndTime'], format="%d.%m.%Y %H:%M")
+        return df
     
     def handleMissing(self, data):
         data["Currency"] = data["Currency"].fillna("EUR") # replace missing value by EUR in Currency
@@ -31,7 +38,22 @@ class Information:
         data = data.dropna(subset=["Day-ahead Price [EUR/MWh]"]) # remove column with missing values
         return data
     
+    def draw(self, df, startdate="2016-01-01 00:00:00"):
+        import pandas as pd
+        startdate = pd.to_datetime(startdate)
+        df_filtered = df[df['StartTime'] >= startdate]
+        
+        plt.figure(figsize=(15, 10))
+        plt.plot(df_filtered['StartTime'], df_filtered['Day-ahead Price [EUR/MWh]'], marker='.', linestyle='-')
+        plt.xlabel('Time')
+        plt.ylabel('Day-ahead Price [EUR/MWh]')
+        plt.title('Day-ahead Electricity Price Over Time')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.show()
+    
 if __name__ == "__main__":
     information = Information()
     df = information.data
-    print(df.to_string())
+    # print(df.to_string())
+    information.draw(df, startdate="2024-09-01 00:00:00")
